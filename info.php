bb80402a8120ecf38678f415c16a51454284fe2d
Charlie Root <root@irr.prontosoft.by>
2014-11-27 12:15:42 +0300

    1

---
+++ info.php
@@ -0,0 +1,37 @@
+<html dir="ltr" lang="en-US">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width" />
+<title>Post From Site 3.0 | kelly dwan</title>
+<body class="single single-post postid-203 single-format-standard single-author singular two-column right-sidebar">
+			<div id="comments">
+			    
+				
+				    
+									<div id="respond">
+											<h3 id="reply-title">Leave a Reply <small><a rel="nofollow" id="cancel-comment-reply-link" href="/2012/01/10/post-from-site-3-0/#respond" style="display:none;">Cancel reply</a></small></h3>
+															    <form action="http://me.redradar.net/wp-comments-post.php" method="post" id="commentform">
+																									<p class="comment-notes">Your email address will not be published. Required fields are marked <span class="required">*</span></p>							<p class="comment-form-author"><label for="author">Name</label> <span class="required">*</span><input id="author" name="author" type="text" value="" size="30" aria-required='true' /></p>
+																									
+																									<p class="comment-form-email"><label for="email">Email</label> <span class="required">*</span><input id="email" name="email" type="text" value="" size="30" aria-required='true' /></p>
+																									<p class="comment-form-url"><label for="url">Website</label><input id="url" name="url" type="text" value="" size="30" /></p>
+																															<p class="comment-form-comment"><label for="comment">Comment</label><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>						<p class="form-allowed-tags">You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes:  <code>&lt;a href=&quot;&quot; title=&quot;&quot;&gt; &lt;abbr title=&quot;&quot;&gt; &lt;acronym title=&quot;&quot;&gt; &lt;b&gt; &lt;blockquote cite=&quot;&quot;&gt; &lt;cite&gt; &lt;code&gt; &lt;del datetime=&quot;&quot;&gt; &lt;em&gt; &lt;i&gt; &lt;q cite=&quot;&quot;&gt; &lt;strike&gt; &lt;strong&gt; </code></p>						<p class="form-submit">
+																															
+																																		    <input name="submit" type="submit" id="submit" value="Post Comment" />
+																																						<input type='hidden' name='comment_post_ID' value='203' id='comment_post_ID' />
+																																						<input type='hidden' name='comment_parent' id='comment_parent' value='0' />
+																																									</p>
+																																												<p style="display: none;"><input type="hidden" id="akismet_comment_nonce" name="akismet_comment_nonce" value="625a7d50a8" /></p>					</form>
+																																															    </div><!-- #respond -->
+																																																		    
+																																																		    </div><!-- #comments -->
+																																																		    
+																																																				    
+																																																						</div><!-- #content -->
+																																																						
+																																																							</div><!-- #primary -->
+																																																							
+																																																							
+																																																							    </div><!-- #main -->
+</body>
+</html>
